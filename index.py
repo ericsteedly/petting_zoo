f1c511277a137fdcca9b6d3e46ca93232bf8c6f1
Eric Steedly <ericsteedly@gmail.com>
2024-02-21 14:42:21 -0600

    added feed method and __str__

--- index.py
+++ index.py
@@ -1,28 +1,29 @@
-from datetime import date
 from crawling import Arachnid, Frog, Lizard, Snake, Turtle
 from swimming import Duck, Fish, Swan, Gator, Goose
 from walking import Llama, Goat, Horse, Pig, Donkey
 
 
-miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning")
-jim = Pig("Jim", "pot-belly pig", "morning")
-joe = Goat("Joe", "Billy Goat", "midday")
-jill = Donkey("Jill", "standard donkey", "midday")
-indie = Horse("Indie", "quarter-horse", "afternoon")
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama chow")
+jim = Pig("Jim", "pot-belly pig", "morning", "slop")
+joe = Goat("Joe", "Billy Goat", "midday", "grass")
+jill = Donkey("Jill", "standard donkey", "midday", "hay")
+indie = Horse("Indie", "quarter-horse", "afternoon", "grass")
 
-sarah = Snake("Sarah", "python")
-sam = Lizard("Sam", "Salamander")
-soren = Arachnid("Soren", "Scorpion")
-santo = Frog("Santo", "poison dart frog")
-slim = Turtle("Slim", "giant tortoise")
+sarah = Snake("Sarah", "python", "1 mouse")
+sam = Lizard("Sam", "Salamander", "lizard food")
+soren = Arachnid("Soren", "Scorpion", "4 crickets")
+santo = Frog("Santo", "poison dart frog", "10 flies")
+slim = Turtle("Slim", "giant tortoise", "carrots")
 
-roy = Swan("Roy", "black swan")
-rowdy = Goose("Rowdy", "canadian goose")
-rhonda = Gator("Rhonda", "caiman")
-rufus = Fish("Rufus", "goldfish")
-ricardo = Duck("Ricardo", "mallard")
+roy = Swan("Roy", "black swan", "bird food")
+rowdy = Goose("Rowdy", "canadian goose", "bird food")
+rhonda = Gator("Rhonda", "caiman", "steak")
+rufus = Fish("Rufus", "goldfish", "fish food")
+ricardo = Duck("Ricardo", "mallard", "bird food")
 
-print(f'{indie.name} the {indie.species} is available to pet during the {indie.shift} shift.')
+
+
+print(ricardo)
 
 
 
--- walking/llama.py
+++ walking/llama.py
@@ -1,9 +1,24 @@
 from datetime import date
 
+
 class Llama:
-    def __init__(self, name, species, shift):
+    """initialization of llama animal 
+    """
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
         self.date_added = date.today()
         self.walking = True
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        """method to feed llama 
+        """
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+
+
